8348bbd064dadd1dd1a1a7cf92cc08fe8fa905f6
Brian Kessler <kessler.bm@gmail.com>
2020-06-22 18:32:57 +0200

    Renamed "refreshRate" to specify seconds for clarity.

--- force-app/main/default/lwc/bitcoinEuroPrice/bitcoinEuroPrice.js
+++ force-app/main/default/lwc/bitcoinEuroPrice/bitcoinEuroPrice.js
@@ -3,14 +3,14 @@ import {LightningElement, api, track} from 'lwc';
 const bitcoinPriceProvider = 'https://api.coinbase.com/v2/prices/spot?currency=EUR';
 export default class BitcoinEuroPrice extends LightningElement {
     @api
-    refreshRate;
+    refreshRateInSeconds;
 
     @track
     euroPrice;
 
     connectedCallback() {
         this.fetchPrice();
-        setInterval(this.fetchPrice, this.refreshRate * 1000);
+        setInterval(this.fetchPrice, this.refreshRateInSeconds * 1000);
     }
 
     fetchPrice() {
