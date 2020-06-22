88b8bb0ca574e443b07b6502c384c581a3f19669
Brian Kessler <kessler.bm@gmail.com>
2020-06-22 18:23:02 +0200

    Made refresh rate configurable.

--- force-app/main/default/lwc/bitcoinEuroPrice/bitcoinEuroPrice.js
+++ force-app/main/default/lwc/bitcoinEuroPrice/bitcoinEuroPrice.js
@@ -1,17 +1,21 @@
-import {LightningElement, track} from 'lwc';
+import {LightningElement, api, track} from 'lwc';
 
 const bitcoinPriceProvider = 'https://api.coinbase.com/v2/prices/spot?currency=EUR';
 
 export default class BitcoinEuroPrice extends LightningElement {
+    @api
+    refreshRate;
+
     @track
     euroPrice;
 
     connectedCallback() {
         this.fetchPrice();
-        setInterval(this.fetchPrice, 10000);
+        setInterval(this.fetchPrice, this.refreshRate * 1000);
     }
 
     fetchPrice() {
+
         fetch(bitcoinPriceProvider)
             .then(response => response.json())
             .then(json => {
